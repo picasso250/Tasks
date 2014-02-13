d99e63e064b743fd7aec8196d215065b18d0cd36
Ryan <cumt.xiaochi@gmail.com>
2014-02-13 21:52:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,11 @@
 <?php
 
-/**
- * @author  ryan <cumt.xiaochi@gmail.com>
- * 由王霄池纯粹手写而成
- */
-
-// 打开错误提示，在 SAE 上 ini_set 将不起作用
-ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-define('IN_APP', 1);
-define('APP_ROOT', __DIR__ . DIRECTORY_SEPARATOR);
+require __DIR__.'/config/common.php';
 
-require 'config/common.php';
-
-// if not debug, mute all error reportings
-if (!(defined('DEBUG') ? DEBUG : 0)) {
-    ini_set('display_errors', 0);
-    error_reporting(0);
-}
-
-require 'lib/function.php';
-include 'lib/autoload.php';
+require __DIR__.'/lib/function.php';
+include __DIR__.'/lib/autoload.php';
 
 // 变量初始化
 require 'init.php';
